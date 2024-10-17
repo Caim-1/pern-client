f8a36d4b4d9b7ab76ba7bb53737a40274b1626d2
Szymon Siudzinski <NaktisPL@gmail.com>
2024-10-17 20:11:19 +0200

    Remove unneeded file

--- src/context/appContext.tsx
+++
@@ -1,8 +0,0 @@
-// import ForumContext from './forumContext';
-
-// import { combineComponents } from '.../utils/combineComponents;
-
-// const providers = [
-//   ForumContext,
-// ]
-// export const AppContextProvider = combineComponents(...providers);
